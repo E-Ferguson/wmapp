deda6045caa092ebe71aab732f8c7ccc67e5ffca
Jacob Weiss <jacobaweiss@gmail.com>
2013-03-21 16:30:16 -0400

    renamed all sessions to user sessions in prep for worker login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Wmapps::Application.routes.draw do
   resources :users, :except => [:destroy, :edit]
-  resources :sessions, :only => [:new, :create, :destroy]
+  resources :user_sessions, :only => [:new, :create, :destroy]
 
   root :to => 'pages#home'
 
-  match '/login',   :to => 'sessions#new'
-  match '/logout',  :to => 'sessions#destroy', via: :delete
+  match '/login',   :to => 'user_sessions#new'
+  match '/logout',  :to => 'user_sessions#destroy', via: :delete
   match '/signup',  :to => 'users#new'
 end
